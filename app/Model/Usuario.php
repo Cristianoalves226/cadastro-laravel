b25e5c381dc94fd92c38265fbb5dcebaa52ed593
Cristiano <c.alvesdecampos@gmail.com>
2021-03-17 22:12:57 -0400

    Inicializando projeto cadastro laravel

---
+++ app/Model/Usuario.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+
+class Usuario extends Model
+{
+    //
+    protected $connection = 'mysql';
+    protected $table = 'usuario';
+
+    public static function listar(int $limite)
+    {
+        $sql = self::select([
+            "id",
+            "nome",
+            "email",
+            "data_cadastro"
+        ])
+            ->limit($limite);
+
+        dd($sql->toSql());
+    }
+
+    public function cadastrar(Request $request)
+    {
+        $sql = self::insert([
+            "nome" => $request->input('nome'),
+            "email" => $request->input('email'),
+            "data_cadastro" => DB::raw('NOW()')
+        ]);
+
+        dd($sql->toSql(), $request->all());
+    }
+}
